624bf7b4841f9196b0858edbcb6ae5aec89a670d
ddebowczyk <ddebowczyk@gmail.com>
2026-03-02 13:50:06 +0000

    Events refactoring - prep for 2.0.0 release

--- src/Contracts/CanProvideConfig.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Cognesy\Config\Contracts;
-
-interface CanProvideConfig
-{
-    public function get(string $path, mixed $default = null): mixed;
-    public function has(string $path): bool;
-}
